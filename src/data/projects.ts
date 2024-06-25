f49e61997d1950457b6ed814502d5475bb5c9dbb
Calco Dev <calcopoddev@gmail.com>
2024-06-25 08:42:49 +0300

    Started work on projects page. Adding react.

---
+++ src/data/projects.ts
@@ -0,0 +1,13 @@
+import type { ProjectData } from "./types";
+
+export const projects: ProjectData[] = [
+  {
+    title: "Project 1",
+    description: "Description of project 1",
+    date: "2021-01-01",
+    author: "Author 1",
+    tags: ["tag1", "tag2"],
+    image: "project1.jpg",
+    filename: "project1.md",
+  },
+];
--- src/data/types.ts
+++ src/data/types.ts
@@ -7,3 +7,13 @@ export interface BlogpostData {
   image: string;
   filename: string;
 }
+
+export interface ProjectData {
+  title: string;
+  description: string;
+  date: string;
+  author: string;
+  tags: string[];
+  image: string;
+  filename: string;
+}
